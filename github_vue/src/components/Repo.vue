d29e5cbb3a5d333019f16ba0806ab5b4b1e5c08c
jhonatadev <xxxonata@gmail.com>
2019-04-04 12:52:11 -0300

    Pegando repo da galera sz

--- github_vue/src/components/Repo.vue
+++ github_vue/src/components/Repo.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="card card-body-mb-2">
+  <div class="card card-body mb-2">
     <div class="row">
-      <div class="col-md-6"><a href target="blank"></a></div>
       <div class="col-md-6">
-        <span class="badge badge-primary"></span>
-        <span class="badge badge-primary"></span>
-        <span class="badge badge-primary"></span>
+        <a :href="repo.html_url" target="blank">{{ repo.name }}</a>
+      </div>
+      <div class="col-md-6">
+        <span class="badge badge-primary">Stars: {{ repo.stargazers_count }}</span>
+        <span class="badge badge-secondary">Watch: {{ repo.watchers_count }}</span>
+        <span class="badge badge-success">Forks: {{ repo.forks_count }}</span>
       </div>
     </div>
   </div>
